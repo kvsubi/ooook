7be1dcc34d03b7c266ba737c36aa6d7985973875
Samuel Davis <41790962+NotBruce@users.noreply.github.com>
2024-01-03 19:29:24 +0000

    New Look

--- src/app/commands/page.tsx
+++ src/app/commands/page.tsx
@@ -1,13 +1,33 @@
+"use client"
+import { useState } from "react"
 import styles from "../styles/commands.module.css"
 import commandsData from "../../../public/commands.json"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import { Layers } from "@material-ui/icons"
+import { motion } from "framer-motion"
 
 export default function Refund() {
-    const commandsList = commandsData.map(command => ({
-        name: command.name,
-        category: command.category,
-        description: command.description,
-        permission: command.permission
-    }))
+    const [commandsList, setCommandsList] = useState(
+        commandsData.map(command => ({
+            name: command.name,
+            category: command.category,
+            description: command.description,
+            permission: command.permission,
+            showContent: false // Add showContent property to track individual div's content visibility
+        }))
+    )
+
+    const toggleContent = (index: number) => {
+        setCommandsList(prevCommandsList => {
+            const updatedCommandsList = [...prevCommandsList]
+            updatedCommandsList[index] = {
+                ...updatedCommandsList[index],
+                showContent: !updatedCommandsList[index].showContent
+            }
+            return updatedCommandsList
+        })
+    }
+
     return (
         <section className={styles["content"]}>
             <span className={styles["title"]}>Commands (Temp)</span>
@@ -15,24 +35,49 @@ export default function Refund() {
                 {commandsList.map((command, index) => (
                     <div key={index} className={styles["command"]}>
                         <div className={styles["command-top-content"]}>
-                            <span className={styles["command-title"]}>{command.name}</span>
-                            <span className={styles["command-description"]}>
-                                {command.description}
-                            </span>
-                        </div>
-                        <hr />
-                        <div className={styles["command-content"]}>
-                            <span className={styles["command-section-title"]}>arguments</span>
-                            <div className={styles["command-section"]}>
-                                <span className={styles["command-argument"]}>none</span>
+                            <div className={styles["command-details"]}>
+                                <Layers
+                                    className={
+                                        command.showContent ? styles["selected-command"] : ""
+                                    }
+                                />
+                                <div className={styles["command-details-inner"]}>
+                                    <span className={styles["command-title"]}>{command.name}</span>
+                                    <span className={styles["command-description"]}>
+                                        {command.description}
+                                    </span>
+                                </div>
                             </div>
-                            <span className={styles["command-section-title"]}>permissions</span>
-                            <div className={styles["command-section"]}>
-                                <span className={styles["command-section-element"]}>
-                                    {command.permission ? command.permission : "none"}
-                                </span>
-                            </div>
+                            <button onClick={() => toggleContent(index)}>
+                                <ExpandMoreIcon />
+                            </button>
                         </div>
+                        {command.showContent && (
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ ease: "easeOut", duration: 2 }}
+                                className={styles["command-content-bottom"]}>
+                                <div className={styles["command-section-outer"]}>
+                                    <span className={styles["command-section-title"]}>
+                                        Arguments:
+                                    </span>
+                                    <div className={styles["command-section"]}>
+                                        <span className={styles["command-argument"]}>none</span>
+                                    </div>
+                                </div>
+                                <div className={styles["command-section-outer"]}>
+                                    <span className={styles["command-section-title"]}>
+                                        Permissions:
+                                    </span>
+                                    <div className={styles["command-section"]}>
+                                        <span className={styles["command-section-element"]}>
+                                            {command.permission ? command.permission : "none"}
+                                        </span>
+                                    </div>
+                                </div>
+                            </motion.div>
+                        )}
                     </div>
                 ))}
             </div>
